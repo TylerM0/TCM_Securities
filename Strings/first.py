8914b1ec3d6e044ccb1424b72a32601fc7c89576
Tyler Mattson <02tyler.mattson@gulllakecs.org>
2024-01-25 09:05:35 -0500

    Strings

---
+++ Strings/first.py
@@ -0,0 +1,13 @@
+#!/bin/python3
+
+#Programmer: Tyler Mattson
+#Program: Strings
+
+print("Michigan Wolverines are National Champions\n")
+
+print ("7 + 8 =", 7+8)
+print("")
+print ("3248230934 + 324820219 = "+ str(3248230934+324820219),"\n")
+
+print("""Happy New Year!\n
+everyone, Yaaay!!!""") #""" will print on multiple lines"""
