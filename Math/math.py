df3c3758f8996e34dcfb79c75dcf0d0b5becd953
Tyler Mattson <02tyler.mattson@gulllakecs.org>
2024-01-25 09:06:22 -0500

    Math

---
+++ Math/math.py
@@ -0,0 +1,13 @@
+#Programmer: Tyler Mattson
+#Program: Math
+#Date: 1.10.2024
+
+print(9 * 9) # Multiply
+print(9 + 9) #Addition
+print(9 - 6) #Subtract
+print(9 / 3) #Divide
+print(9 + 9 - 9 * 9 / 9) #PEMDAS
+print(50 ** 21) #Exponent
+print(50 % 6) #Modulus.. 50/6 = 8.. 8x6 = 48. Answer is remainder
+print(50 / 6) #Division with remainder - Float/Decimal
+print(50 // 6) #Division with no remainder
